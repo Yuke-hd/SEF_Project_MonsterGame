051f77ed6af6efbb185994ffc8510ac2f1fa8141
ke yu <tyrantviru@gmail.com>
2018-08-11 20:38:14 +1000

    User login & sign up

--- src/monsterGame/Login.java
+++ src/monsterGame/Login.java
@@ -23,16 +23,19 @@ public class Login extends JFrame {
 		User administrator = new User("admin", "password".toCharArray());
 		admin.userList.add(administrator);
 		// 创建 JFrame 实例
-		JFrame frame = new JFrame("Login Example");
+		JFrame frame = new JFrame("Login");
 		// Setting the width and height of frame
+		frame.setLocationRelativeTo(null);
 		frame.setSize(350, 200);
+		frame.setResizable(false); 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		/*
-		 * 创建面板，这个类似于 HTML 的 div 标签 我们可以创建多个面板并在 JFrame 中指定位置 面板中我们可以添加文本字段，按钮及其他组件。
+		 * 创建面板，这个类似于 HTML 的 </div> 标签 我们可以创建多个面板并在 JFrame 中指定位置 面板中我们可以添加文本字段，按钮及其他组件。
 		 */
 		JPanel panel = new JPanel();
 		// 添加面板
 		frame.add(panel);
+		panel.setLocation(1000, 100);
 		/*
 		 * 调用用户定义的方法并添加组件到面板
 		 */
@@ -54,36 +57,36 @@ public class Login extends JFrame {
 		 * 这个方法定义了组件的位置。 setBounds(x, y, width, height) x 和 y 指定左上角的新位置，由 width 和 height
 		 * 指定新的大小。
 		 */
-		userLabel.setBounds(10, 20, 80, 25);
+		userLabel.setBounds(40, 20, 80, 25);
 		panel.add(userLabel);
 
 		/*
 		 * 创建文本域用于用户输入
 		 */
 		// JTextField userText = new JTextField(20);
-		_userText.setBounds(100, 20, 165, 25);
+		_userText.setBounds(130, 20, 165, 25);
 		panel.add(_userText);
 
 		// 输入密码的文本域
 		JLabel passwordLabel = new JLabel("Password:");
-		passwordLabel.setBounds(10, 50, 80, 25);
+		passwordLabel.setBounds(40, 50, 80, 25);
 		panel.add(passwordLabel);
 
 		/*
 		 * 这个类似用于输入的文本域 但是输入的信息会以点号代替，用于包含密码的安全性
 		 */
 		// JPasswordField passwordText = new JPasswordField(20);
-		_passwordText.setBounds(100, 50, 165, 25);
+		_passwordText.setBounds(130, 50, 165, 25);
 		panel.add(_passwordText);
 
 		// 创建登录按钮
 		JButton loginButton = new JButton("login");
-		loginButton.setBounds(10, 90, 90, 25);
+		loginButton.setBounds(60, 90, 90, 25);
 		panel.add(loginButton);
 
 		// 创建注册按钮
 		JButton signButton = new JButton("sign up");
-		signButton.setBounds(130, 90, 90, 25);
+		signButton.setBounds(180, 90, 90, 25);
 		panel.add(signButton);
 
 		// 创建监视器
@@ -113,7 +116,7 @@ public class Login extends JFrame {
 				JOptionPane.showMessageDialog(Jpanel, "账号密码错误", "登陆验证", JOptionPane.WARNING_MESSAGE);
 				_userText.setText("");_passwordText.setText("");
 			}
-		}
+		} 
 	}
 
 	private boolean auth(String userName, char[] pwd) {
--- src/monsterGame/SignUp.java
+++ src/monsterGame/SignUp.java
@@ -16,6 +16,7 @@ public class SignUp extends JFrame {
 	User _user;
 
 	JFrame frame = new JFrame("Sign Up");
+	
 
 	public SignUp() {
 		// TODO Auto-generated constructor stub
@@ -24,7 +25,9 @@ public class SignUp extends JFrame {
 	public User signUp0() {
 		// SignUp signUp = new SignUp();
 		// Setting the width and height of frame
+		frame.setLocationRelativeTo(null);
 		frame.setSize(350, 200);
+		frame.setResizable(false);
 		// frame.setDefaultCloseOperation();
 		/*
 		 * 创建面板，这个类似于 HTML 的 div 标签 我们可以创建多个面板并在 JFrame 中指定位置 面板中我们可以添加文本字段，按钮及其他组件。
